77c57501f6f8f350f25f226befedc35b11dd9dad
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2020-08-16 15:20:20 +0200

    Assignment 2.1

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,7 +1,47 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader , Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import RenderLeader from './RenderLeaderComponent';
+
+
+
+const RenderLeader = (props) =>  {
+
+    const leader = props.leaders.map((leader)=>{
+        return(
+            <Media key={leader.id} list>
+                <Media tag="li">
+                    <Media left className="mr-3 ">
+                        <Media src={leader.image} alt="" />
+                    </Media>
+                    <Media body>
+                        <Media heading>
+                            {leader.name}
+                            
+                        </Media>
+                        <Media body>
+                        {leader.designation}
+                        </Media>
+                        <br/>
+                        <Media body>
+                        {leader.description}
+                        </Media>
+                    </Media>
+                </Media>
+            </Media>
+        );
+    })
+
+
+    return(
+        <div>
+            {leader}
+        </div>
+    );
+
+}
+
+
+
 
 function About(props) {
 
